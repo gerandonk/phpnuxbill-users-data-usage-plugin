948dbeae99049747fa2b9a6237b457cf1ead7407
ABDALRAHMAN MOLOOD <amolood@icloud.com>
2024-08-10 06:11:35 +0300

    Update data_usage_user.php

--- data_usage_user.php
+++ data_usage_user.php
@@ -13,7 +13,7 @@ function UserDataUsage()
     $page = !isset($_GET['page']) ? 1 : (int)$_GET['page'];
     $perPage = 10;
 
-    $data = fetch_user_in_out_data($search, $page, $perPage);
+    $data = fetch_user_in_out_data_admin($search, $page, $perPage);
     $total = count_user_in_out_data($search);
     $pagination = create_pagination($page, $perPage, $total);
 
